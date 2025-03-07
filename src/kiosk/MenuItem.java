91cc07f425d790e843abaf33dd29ac416e7beb98
janghwal <jhp337037@gmail.com>
2025-03-07 11:40:59 +0900

    level2

--- src/kiosk/MenuItem.java
+++ src/kiosk/MenuItem.java
@@ -1,5 +1,24 @@
 package kiosk;
 
 public class MenuItem {
+    private String productName;
+    private Double productPrice;
+    private String productInfo;
+
+    MenuItem(String productName, Double productPrice, String productInfo){
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productInfo = productInfo;
+    }
+
+    public String getProductName(){
+        return productName;
+    }
+    public Double getProductPrice(){
+        return productPrice;
+    }
+    public String getProductInfo(){
+        return productInfo;
+    }
 
 }
